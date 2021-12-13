84393517605c8f1f7bcd77a98dd284257fe9be83
Diego Serafico <dseraficohernandez@gmail.com>
2021-12-12 22:56:29 -0800

    Research on web scraping and add proposal txt

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../App.css';
+import Header from './Header';
 
 function App() {
   return (
     <React.Fragment>
+      <Header />
     </React.Fragment>
   );
 }
