37b51187b758e4a27c937de4fc3a52394790e844
Diego Serafico <dseraficohernandez@gmail.com>
2021-12-21 01:38:22 -0800

    temporary commit

--- src/components/EditWishForm.js
+++ src/components/EditWishForm.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import ReusableForm from './ReusableForm'
+import PropTypes from "prop-types";
+import { useNavigate } from 'react-router-dom';
 
-function EditWishForm () {
+function EditWishForm (props) {
+  let navigate = useNavigate();
+  function handleUpdateWishForm(e) {
+    e.preventDefault();
+    props.onUpdateWish({
+      name: e.target.name.value,
+      image: e.target.image.value,
+      url: e.target.url.value,
+      price: e.target.price.value,
+      description: e.target.description.value
+    });
+    navigate('/wishlist');
+  }
   return (
     <React.Fragment>
-      <ReusableForm />
+      <ReusableForm
+        formSubmissionHandler={handleUpdateWishForm}
+        buttonText='Update'
+       />
     </React.Fragment>
   )
+}
+
+EditWishForm.propTypes = {
+  onUpdateWish: PropTypes.func
 }
 
 export default EditWishForm
